dbb07247b229ddbbcd3f675c3c2e2f69b6fdf83f
Lokesh shinde <lokeshinde500@gmail.com>
2025-05-19 22:28:16 +0530

    day 1 python basics

---
+++ DAY_01/hello.py
@@ -0,0 +1,54 @@
+#number
+age = 10 
+
+# string
+name = "Anurag"
+
+# boolean
+isVerified = True
+
+# float
+pi = 3.14
+
+# list
+users = ["Lokesh" , "Jayesh" , "Nikhil", "Nikhil"]
+
+# tuple
+fruits = ("Mango", "Apple", "Cherry")
+
+# set
+numbers = {1,4,5,6,65,6}
+
+# dictionary
+userData = {
+  "name": "Lokesh",
+  "age": 24,
+  "role": "Developer"
+}
+
+
+# type conversion
+value = "10.5"
+num  = float(value)
+num2 = int(num)
+name = str(num2)
+
+#take  input from user
+name = input("Enter your name: ")
+
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+sum = a + b 
+
+print( a, "+" , b, "=", sum)
+
+print ("Python", "is", "awesome" , sep="_" , end="!")
+
+print(numbers)
+
+a = int(10)
+b = float(10.5)
+c = a + b
+
+print(numbers)
