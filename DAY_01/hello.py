1bf79e923bc58e1a3cc07690cef74b743f581e6f
Lokesh shinde <lokeshinde500@gmail.com>
2025-05-19 22:37:21 +0530

    Day 01 python

--- DAY_01/hello.py
+++ DAY_01/hello.py
@@ -1,54 +1,95 @@
-#number
-age = 10 
+# PYTHON DATA TYPES
+# 1. String
+# 2. Integer
+# 3. Float
+# 4. Boolean
+# 5. List
+# 6. Tuple
+# 7. Set
+# 8. Dictionary
 
-# string
+
+# 1. String
 name = "Anurag"
+print(name)
+print(type(name))
 
-# boolean
-isVerified = True
+# 2. Integer
+age = 10
+print(age)
+print(type(age))
 
-# float
+# 3. Float
 pi = 3.14
+print(pi)
+print(type(pi))
 
-# list
+# 4. Boolean
+isVerified = True
+print(isVerified)
+print(type(isVerified))
+
+
+# 5. List
 users = ["Lokesh" , "Jayesh" , "Nikhil", "Nikhil"]
+print(users)
+print(type(users))
 
-# tuple
+# 6. Tuple
 fruits = ("Mango", "Apple", "Cherry")
+print(fruits)
+print(type(fruits))
 
-# set
+# 7. Set
 numbers = {1,4,5,6,65,6}
+print(numbers)
+print(type(numbers))
 
-# dictionary
+# 8. Dictionary
 userData = {
   "name": "Lokesh",
   "age": 24,
   "role": "Developer"
 }
+print(userData)
+print(type(userData))
 
 
 # type conversion
-value = "10.5"
-num  = float(value)
-num2 = int(num)
-name = str(num2)
 
-#take  input from user
-name = input("Enter your name: ")
+# 1. str()
+# 2. int()
+# 3. float()
+# 4. bool()
+# 5. list()
+# 6. tuple()
+# 7. set()
+# 8. dict()
 
 
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-sum = a + b 
+# 1. str()
+age = 10
+print(type(age))
+age = str(age)
+print(type(age))
 
-print( a, "+" , b, "=", sum)
+# 2. int()
+age = "10"
+print(type(age))
+age = int(age)
+print(type(age))
 
-print ("Python", "is", "awesome" , sep="_" , end="!")
+# 3. float()
+age = 10
+print(type(age))
+age = float(age)
+print(type(age))
 
-print(numbers)
 
-a = int(10)
-b = float(10.5)
-c = a + b
+# 4. bool()
+age = 10
+print(type(age))
+age = bool(age)
+print(type(age))
 
-print(numbers)
+
