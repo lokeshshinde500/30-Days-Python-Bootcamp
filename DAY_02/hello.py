d0fc368ee8adb93d48decd470cadeab33d396578
Lokesh shinde <lokeshinde500@gmail.com>
2025-05-21 21:13:29 +0530

    operators in python

--- DAY_02/hello.py
+++ DAY_02/hello.py
@@ -1,2 +1,81 @@
-# DAY 02 FOR 30 DAYS OF PYTHON
+# Day 02 of python 30 day.
+
+# arithmatic operator
+
+a = 10
+b = 5
+
+# addition
+print(a+b)
+
+# subtraction
+print(a-b)
+
+# multiplication
+print(a*b)
+
+# division
+print(a/b)
+
+# modulus
+print(a%b)
+
+# floor division
+print(a//b)
+
+c,d = 2,5 
+# exponent
+print(c**d)
+
+# comparison operator
+
+print(a==b)
+print(a!=b)
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+
+# logical operator
+print("-"*20)
+print("logical operator")
+print(a and b)
+print(a or b)
+print(not a)
+
+
+# assignment operator
+print("-"*20)
+print("assignment operator")
+
+value1 , value2 = 1, 2
+print(value1 , value2)
+
+value1 , value2 = value2 , value1
+print(value1 , value2)
+
+value1+=1
+print(value1)
+
+value1-=1
+print(value1)
+
+value1*=2
+print(value1)
+
+value1/=2
+print(value1)
+
+value1 = 4
+value1%=2
+print(value1)
+
+value1 = 4
+value1**=2
+print(value1)
+
+value1 = 4
+value1//=2
+print(value1)
+
 
